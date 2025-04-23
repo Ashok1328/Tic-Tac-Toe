a0eccfc05460275e5368c18e3e6e410f89a86725
Ashok1328 <ashokkhatiwada1990@gmail.com>
2025-04-23 14:05:10 +0545

    new repo

---
+++ first.js
@@ -0,0 +1,91 @@
+let boxes = document.querySelectorAll(".box");
+let newGameBtn = document.querySelector("#new-btn");
+let resetBtn = document.querySelector("#reset-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let draw = document.querySelector("#draw");
+
+let turn0 = true;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turn0 = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.innerText !== "") return;
+
+    if (turn0) {
+      box.innerText = "0";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations!! Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  let winnerFound = false;
+
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        winnerFound = true;
+        return;
+      }
+    }
+  }
+
+  if (!winnerFound) {
+    let allFilled = Array.from(boxes).every((box) => box.innerText !== "");
+    if (allFilled) {
+      msg.innerText = "It's a draw";
+      msgContainer.classList("hide");
+      disableBoxes();
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
